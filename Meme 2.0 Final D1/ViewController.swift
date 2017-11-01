12440f3f9ede971484d361d5ef8dba4759b5806f
Yugandhara Lad <yugandharalad9@gmail.com>
2017-11-01 10:42:28 -0400

    Initial Commit

---
+++ Meme 2.0 Final D1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme 2.0 Final D1
+//
+//  Created by Yugandhara Lad on 11/1/17.
+//  Copyright © 2017 Yugandhara Lad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
